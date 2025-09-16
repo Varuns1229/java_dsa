2066873f8e2c8d6e4bb00f28b5a5fcb8aa6c5fd3
varun singh <varuns12.29@gmail.com>
2025-09-16 23:23:27 +0530

    subsets of array

---
+++ array/subsetsOfArray.java
@@ -0,0 +1,56 @@
+package array;
+
+import java.util.Scanner;
+
+public class subsetsOfArray {
+
+    public static int[] binaryArray(int num, int length) {
+        int[] binArry = new int[length];
+        int i = length;
+
+        while (num > 0) {
+            int rim = num % 2;
+            binArry[i - 1] = rim;
+            num = num / 2;
+            i--;
+        }
+
+        return binArry;
+
+    }
+
+    public static void subsetsPrint(int[] arr1) {
+        int n = arr1.length;
+        int pow = 1;
+        while (n > 0) {
+            pow = pow * 2;
+            n--;
+        }
+        for (int i = 0; i < pow; i++) {
+            int[] binArry = binaryArray(i, arr1.length);
+            for (int j = 0; j < binArry.length; j++) {
+                if (binArry[j] == 1)
+                    System.out.print(arr1[j] + " ");
+            }
+            System.out.print(" \n");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        try (Scanner scn = new Scanner(System.in)) {
+            int n = scn.nextInt();
+            int[] arr = new int[n];
+
+            // create array
+            for (int i = 0; i < n; i++) {
+                int num = scn.nextInt();
+                arr[i] = num;
+            }
+
+            subsetsPrint(arr);
+
+        }
+    }
+
+}
